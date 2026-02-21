abd1dea3d3fc21757750198e6d2bd752953a5863
amandeepsingh0045 <amandeepsingh0045@gmail.com>
2026-02-22 00:50:42 +0530

    added Binary Tree Code

---
+++ src/main/java/com/practice/trees/BinarySearchTree.java
@@ -0,0 +1,36 @@
+package com.practice.trees;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args){
+
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+        intTree.insert(22);
+        intTree.insert(32);
+      //   intTree.insert(17);
+
+
+        intTree.traverseInOrder();
+        System.out.println();
+        System.out.println(intTree.get(27));
+        System.out.println(intTree.get(2744));
+
+        System.out.println(intTree.min());
+        System.out.println(intTree.max());
+
+        intTree.delete(88);
+        intTree.traverseInOrder();
+        System.out.println();
+        intTree.traversePreOrder();
+    }
+
+
+
+}
